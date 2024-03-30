b914883b97ee43505c8de94ea5ce950ff3c9655e
Zeeshan Mukhtar <91063160+ZeeshanMukhtar1@users.noreply.github.com>
2024-03-30 20:00:37 +0500

    👽️ added logout user api

---
+++ src/app/api/users/logout/route.ts
@@ -0,0 +1,24 @@
+import { connectToDB } from '@/db/dbConfig';
+import { NextRequest, NextResponse } from 'next/server';
+
+// connting to db
+connectToDB;
+
+export async function POST(request: NextRequest) {
+  try {
+    const response = NextResponse.json({
+      message: 'Logged out successfully',
+      success: true,
+    });
+
+    // setting cookie
+    response.cookies.set('token', '', {
+      httpOnly: true,
+      expires: new Date(0),
+    });
+
+    return response;
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
